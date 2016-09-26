4c37a1177589d5fcdd4822a22d63ee792562f756
Nikit Begwani <nikitbegwani@gmail.com>
2016-09-27 03:09:59 +0530

    Divide added

--- Assignment2/Assignment2-Part1.pl
+++ Assignment2/Assignment2-Part1.pl
@@ -6,15 +6,21 @@ minus(M,N,R):- length(N,X),		/**find the differnece between both the list, with 
 			   length(P,X),
 			   append(P,R,M).
 
-
 first(L, A) :-				/**Used by multiplication to halve the list**/
     append(A, B, L),
     length(A, N),
     length(B, N).
-
-
+	
 multiply(M,N,A):- findall([X,Y],					/**Cartesian Product**/
 				  (member(X,M),member(Y,N)),R),
 				  flatten(R,X),						/**flatten the list of list to single list**/
 				  first(X,A).						/**take halve the elements because cartesian product gives double number of elements**/
 
+part([],_).				/**base case for splitting**/
+part(L,N):-											/**Recursive call for splitting list in chunks of length N**/
+					length(DL,N),					/**Initialising DL with length N**/
+					append(DL,LTAIL,L),				/**Splitting list into N and length(list)-N**/
+					part(LTAIL,N).					/**Recursively splitting the remaining list**/
+
+divide(M,N):-length(N,R),							/**Divide Function**/
+			part(M,R).
